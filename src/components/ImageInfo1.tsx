5155a988204d8ad01ff58893cb9a03ff22e96839
noaprost <kangjiwon1850@gmail.com>
2024-05-10 03:40:49 +0900

    feat: 디자인 수정

--- src/components/ImageInfo1.tsx
+++ src/components/ImageInfo1.tsx
@@ -6,17 +6,17 @@ export default function ImageInfo1() {
   return (
     <section className="mt-96 text-3xl flex flex-col items-center gap-20">
       <div className="flex justify-center items-center gap-20">
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center gap-3">
           <p>기존 영상을 업로드 하고</p>
-          <div className="w-72 h-72 border-4 border-neutral-400 rounded-xl flex justify-center items-center bg-amber-100">
+          <div className="w-72 h-48 border-4 border-neutral-400 rounded-xl flex justify-center items-center bg-amber-100">
             <PlusCircleIcon />
           </div>
           <p className="text-neutral-700 text-2xl">또는 영상 링크</p>
         </div>
         <PlusIcon />
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center gap-3">
           <p>풀영상을 업로드 하면</p>
-          <div className="w-72 h-72 border-4 border-neutral-400 rounded-xl flex justify-center items-center bg-blue-100">
+          <div className="w-72 h-48 border-4 border-neutral-400 rounded-xl flex justify-center items-center bg-blue-100">
             <PlusCircleIcon />
           </div>
           <p className="text-white">o</p>
