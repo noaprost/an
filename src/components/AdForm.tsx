a35841268e851f0d61b3f670d22020c207911a51
noaprost <kangjiwon1850@gmail.com>
2024-05-10 15:03:23 +0900

    feat:디자인 수정

--- src/components/AdForm.tsx
+++ src/components/AdForm.tsx
@@ -7,7 +7,7 @@ export default function AdForm() {
     // 채널에 사용자를 추가
   };
   return (
-    <section className="mt-60 flex flex-col items-center text-4xl">
+    <section className="mt-52 flex flex-col items-center text-4xl">
       <div className="flex gap-3">
         <KakaoIcon />
         <p>플러스 친구를 추가하고 AN 출시 소식을 받아보세요</p>
@@ -17,7 +17,7 @@ export default function AdForm() {
         <span className="text-blue-900">3개월 무료 이용권</span>을 드려요
       </p>
       <button
-        className="bg-gradient-to-tr from-amber-200 via-emerald-400 to-blue-400 px-5 py-2 text-3xl rounded-xl mt-16 text-black outline-none hover:from-amber-300 hover:via-emerald-500 hover:to-blue-500"
+        className="bg-gradient-to-tr from-amber-200 via-emerald-400 to-blue-400 px-5 py-2 text-3xl rounded-xl mt-16 text-black outline-none hover:from-amber-300 hover:via-emerald-500 hover:to-blue-500 shadow-lg"
         onClick={handleClick}
       >
         AN 카톡 친구 추가하기
